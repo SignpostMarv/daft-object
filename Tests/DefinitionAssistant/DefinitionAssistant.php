3176db9895cb04bafcc79b5613f0f90f0df50443
SignpostMarv <me@signpostmarv.name>
2019-02-20 00:22:57 +0000

    php-cs-fixer

--- Tests/DefinitionAssistant/DefinitionAssistant.php
+++ Tests/DefinitionAssistant/DefinitionAssistant.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
 
 namespace SignpostMarv\DaftObject\Tests\DefinitionAssistant;
 
-use Closure;
 use SignpostMarv\DaftObject\DaftObject;
 use SignpostMarv\DaftObject\DefinitionAssistant as Base;
 
