b01a7be281ec9e8b4bd9c420bbd23e74ba66e97e
SignpostMarv <me@signpostmarv.name>
2019-02-27 22:33:24 +0000

    fleshing out tests

--- Tests/AbstractArrayBackedDaftObject/AbstractArrayBackedDaftObjectTest.php
+++ Tests/AbstractArrayBackedDaftObject/AbstractArrayBackedDaftObjectTest.php
@@ -10,6 +10,7 @@ namespace SignpostMarv\DaftObject\Tests\AbstractArrayBackedDaftObject;
 
 use PHPUnit\Framework\TestCase as Base;
 use SignpostMarv\DaftObject\Exceptions\PropertyNotNullableException;
+use SignpostMarv\DaftObject\Exceptions\PropertyValueNotOfExpectedTypeException;
 
 class AbstractArrayBackedDaftObjectTest extends Base
 {
@@ -25,5 +26,282 @@ class AbstractArrayBackedDaftObjectTest extends Base
         );
 
         $obj->public_RetrievePropertyValueFromData('foo');
+    }
+
+    public function dataProvider_RetrievePropertyValueFromDataExpectStringOrNull() : array
+    {
+        return [
+            [
+                1,
+                (
+                    'Property not of expected type string: ' .
+                    Fixtures\AbstractArrayBackedDaftObject::class .
+                    '::$allows_null'
+                ),
+            ],
+        ];
+    }
+
+    public function dataProvider_RetrievePropertyValueFromDataExpectArrayOrNull() : array
+    {
+        return [
+            [
+                1,
+                (
+                    'Property not of expected type array: ' .
+                    Fixtures\AbstractArrayBackedDaftObject::class .
+                    '::$allows_null'
+                ),
+            ],
+        ];
+    }
+
+    public function dataProvider_RetrievePropertyValueFromDataExpectIntishOrNull() : array
+    {
+        return [
+            [
+                1.0,
+                (
+                    'Property not of expected type int: ' .
+                    Fixtures\AbstractArrayBackedDaftObject::class .
+                    '::$allows_null'
+                ),
+            ],
+            [
+                1,
+                null,
+            ],
+            [
+                '1',
+                null,
+            ],
+        ];
+    }
+
+    public function dataProvider_RetrievePropertyValueFromDataExpectFloatishOrNull() : array
+    {
+        return [
+            [
+                1,
+                (
+                    'Property not of expected type float: ' .
+                    Fixtures\AbstractArrayBackedDaftObject::class .
+                    '::$allows_null'
+                ),
+            ],
+            [
+                1.0,
+                null,
+            ],
+            [
+                '1.0',
+                null,
+            ],
+        ];
+    }
+
+    public function dataProvider_RetrievePropertyValueFromDataExpectBoolishOrNull() : array
+    {
+        return [
+            [
+                [],
+                (
+                    'Property not of expected type bool: ' .
+                    Fixtures\AbstractArrayBackedDaftObject::class .
+                    '::$allows_null'
+                ),
+            ],
+            [
+                1,
+                null,
+            ],
+            [
+                '1',
+                null,
+            ],
+            [
+                0,
+                null,
+            ],
+            [
+                '0',
+                null,
+            ],
+            [
+                true,
+                null,
+            ],
+            [
+                false,
+                null,
+            ],
+        ];
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    *
+    * @dataProvider dataProvider_RetrievePropertyValueFromDataExpectStringOrNull
+    */
+    public function test_RetrievePropertyValueFromDataExpectStringOrNull(
+        $val,
+        ? string $exception_message
+    ) : void {
+        $obj = new Fixtures\AbstractArrayBackedDaftObject();
+
+        static::assertNull($obj->public_RetrievePropertyValueFromDataExpectStringOrNull(
+            'allows_null',
+            $val
+        ));
+
+        $obj = new Fixtures\AbstractArrayBackedDaftObject([
+            'allows_null' => $val,
+        ]);
+
+        if (is_string($exception_message)) {
+            static::expectException(PropertyValueNotOfExpectedTypeException::class);
+            static::expectExceptionMessage($exception_message);
+        }
+
+        $out = $obj->public_RetrievePropertyValueFromDataExpectStringOrNull('allows_null', $val);
+
+        if (is_null($exception_message)) {
+            static::assertIsString($out);
+        }
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    *
+    * @dataProvider dataProvider_RetrievePropertyValueFromDataExpectArrayOrNull
+    */
+    public function test_RetrievePropertyValueFromDataExpectArrayOrNull(
+        $val,
+        ? string $exception_message
+    ) : void {
+        $obj = new Fixtures\AbstractArrayBackedDaftObject();
+
+        static::assertNull($obj->public_RetrievePropertyValueFromDataExpectArrayOrNull(
+            'allows_null',
+            $val
+        ));
+
+        $obj = new Fixtures\AbstractArrayBackedDaftObject([
+            'allows_null' => $val,
+        ]);
+
+        if (is_string($exception_message)) {
+            static::expectException(PropertyValueNotOfExpectedTypeException::class);
+            static::expectExceptionMessage($exception_message);
+        }
+
+        $out = $obj->public_RetrievePropertyValueFromDataExpectArrayOrNull('allows_null', $val);
+
+        if (is_null($exception_message)) {
+            static::assertIsArray($out);
+        }
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    *
+    * @dataProvider dataProvider_RetrievePropertyValueFromDataExpectIntishOrNull
+    */
+    public function test_RetrievePropertyValueFromDataExpectIntishOrNull(
+        $val,
+        ? string $exception_message
+    ) : void {
+        $obj = new Fixtures\AbstractArrayBackedDaftObject();
+
+        static::assertNull($obj->public_RetrievePropertyValueFromDataExpectIntishOrNull(
+            'allows_null',
+            $val
+        ));
+
+        $obj = new Fixtures\AbstractArrayBackedDaftObject([
+            'allows_null' => $val,
+        ]);
+
+        if (is_string($exception_message)) {
+            static::expectException(PropertyValueNotOfExpectedTypeException::class);
+            static::expectExceptionMessage($exception_message);
+        }
+
+        $out = $obj->public_RetrievePropertyValueFromDataExpectIntishOrNull('allows_null', $val);
+
+        if (is_null($exception_message)) {
+            static::assertIsInt($out);
+        }
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    *
+    * @dataProvider dataProvider_RetrievePropertyValueFromDataExpectFloatishOrNull
+    */
+    public function test_RetrievePropertyValueFromDataExpectFloatishOrNull(
+        $val,
+        ? string $exception_message
+    ) : void {
+        $obj = new Fixtures\AbstractArrayBackedDaftObject();
+
+        static::assertNull($obj->public_RetrievePropertyValueFromDataExpectFloatishOrNull(
+            'allows_null',
+            $val
+        ));
+
+        $obj = new Fixtures\AbstractArrayBackedDaftObject([
+            'allows_null' => $val,
+        ]);
+
+        if (is_string($exception_message)) {
+            static::expectException(PropertyValueNotOfExpectedTypeException::class);
+            static::expectExceptionMessage($exception_message);
+        }
+
+        $out = $obj->public_RetrievePropertyValueFromDataExpectFloatishOrNull('allows_null', $val);
+
+        if (is_null($exception_message)) {
+            static::assertIsFloat($out);
+        }
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    *
+    * @dataProvider dataProvider_RetrievePropertyValueFromDataExpectBoolishOrNull
+    */
+    public function test_RetrievePropertyValueFromDataExpectBoolishOrNull(
+        $val,
+        ? string $exception_message
+    ) : void {
+        $obj = new Fixtures\AbstractArrayBackedDaftObject();
+
+        static::assertNull($obj->public_RetrievePropertyValueFromDataExpectBoolishOrNull(
+            'allows_null',
+            $val
+        ));
+
+        $obj = new Fixtures\AbstractArrayBackedDaftObject([
+            'allows_null' => $val,
+        ]);
+
+        if (is_string($exception_message)) {
+            static::expectException(PropertyValueNotOfExpectedTypeException::class);
+            static::expectExceptionMessage($exception_message);
+        }
+
+        $out = $obj->public_RetrievePropertyValueFromDataExpectBoolishOrNull('allows_null', $val);
+
+        if (is_null($exception_message)) {
+            static::assertIsBool($out);
+
+            if (is_bool($val)) {
+                static::assertIsBool($obj->public_RetrievePropertyValueFromDataExpectBoolish(
+                    'allows_null',
+                    $val
+                ));
+            }
+        }
     }
 }
--- Tests/AbstractArrayBackedDaftObject/Fixtures/AbstractArrayBackedDaftObject.php
+++ Tests/AbstractArrayBackedDaftObject/Fixtures/AbstractArrayBackedDaftObject.php
@@ -10,11 +10,75 @@ use SignpostMarv\DaftObject\AbstractArrayBackedDaftObject as Base;
 
 class AbstractArrayBackedDaftObject extends Base
 {
+    const NULLABLE_PROPERTIES = [
+        'allows_null',
+    ];
+
     /**
     * @return scalar|array|object|null
     */
     public function public_RetrievePropertyValueFromData(string $property)
     {
         return $this->RetrievePropertyValueFromData($property);
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    */
+    public function public_RetrievePropertyValueFromDataExpectStringOrNull(
+        string $property,
+        $val
+    ) {
+        return $this->RetrievePropertyValueFromDataExpectStringOrNull($property, $val);
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    */
+    public function public_RetrievePropertyValueFromDataExpectArrayOrNull(
+        string $property,
+        $val
+    ) {
+        return $this->RetrievePropertyValueFromDataExpectArrayOrNull($property, $val);
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    */
+    public function public_RetrievePropertyValueFromDataExpectIntishOrNull(
+        string $property,
+        $val
+    ) {
+        return $this->RetrievePropertyValueFromDataExpectIntishOrNull($property, $val);
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    */
+    public function public_RetrievePropertyValueFromDataExpectFloatishOrNull(
+        string $property,
+        $val
+    ) {
+        return $this->RetrievePropertyValueFromDataExpectFloatishOrNull($property, $val);
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    */
+    public function public_RetrievePropertyValueFromDataExpectBoolishOrNull(
+        string $property,
+        $val
+    ) {
+        return $this->RetrievePropertyValueFromDataExpectBoolishOrNull($property, $val);
+    }
+
+    /**
+    * @param scalar|array|object|null $val
+    */
+    public function public_RetrievePropertyValueFromDataExpectBoolish(
+        string $property,
+        $val
+    ) {
+        return $this->RetrievePropertyValueFromDataExpectBoolish($property, $val);
     }
 }
